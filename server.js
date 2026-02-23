44f4cc554e637ad2ebf2dc011b101d832b4b0baf
emna2003 <157595278+emna2003@users.noreply.github.com>
2026-02-23 11:12:17 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const { SerialPort } = require('serialport');
-const { ReadlineParser } = require('@serialport/parser-readline');
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, { cors: { origin: "*" } });
 
-app.use(express.static(__dirname)); 
-
-// --- CONFIGURATION PORT SÉRIE ---
-// Remplace 'COM3' par ton port (ex: '/dev/tty.usbserial-110' sur Mac)
-const portPath = 'COM3'; 
-const port = new SerialPort({ path: portPath, baudRate: 115200 });
-const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
-
-parser.on('data', (data) => {
-    const edaValue = parseInt(data.trim());
-    if (!isNaN(edaValue)) {
-        io.emit('eda-data', edaValue);
-    }
+io.on('connection', (socket) => {
+    console.log('Un appareil est connecté');
+    
+    // Relais : Reçoit de Python -> Envoie au HTML
+    socket.on('sensor_data', (data) => {
+        socket.broadcast.emit('sensor_data', data);
+    });
 });
 
-server.listen(3000, () => {
-    console.log('Serveur actif sur http://localhost:3000');
-});
+http.listen(3000, () => console.log('Serveur sur port 3000'));
